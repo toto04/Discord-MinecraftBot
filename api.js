1b7ea1a48e57d9cba25392fbba5131c45ba2ec71
Tommaso Morganti <totoscemo@gmail.com>
2019-06-26 04:30:55 +0200

    start button

--- api.js
+++ api.js
@@ -6,7 +6,8 @@ app.get('/list', (req, res) => {
 })
 
 app.post('/start', (req, res) => {
-    res.send()
+    res.send('ciao')
+    console.log('Server starting...')
 })
 
 app.use(express.static('static'))
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,10 @@
 window.addEventListener('load', () => {
-
+    document.getElementById('start').addEventListener('click', async () => {
+        var started = await fetch('/start', {
+            method: 'post'
+        })
+        console.log(await started.json())
+    })
 })
 
 window.addEventListener('resize', () => {
